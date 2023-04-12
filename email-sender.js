fd683174225462db0629126f6de59b11f5bcaa1d
Saghroayoub <ayoubsaghro27@gmail.com>
2023-04-12 13:54:50 +0000

    modifie email

--- email-sender.js
+++ email-sender.js
@@ -4,15 +4,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'ayoubsaghro27@gmail.com',
-        pass: 'ayoub1234'
+        user: 'ayoubgmail.com',
+        pass: '........'
     }
 });
 
 // configure email details
 const mailOptions = {
-    from: 'ayoubsaghro27@gmail.com',
-    to: 'ayoub.saghro@edu.uiz.ac.ma',
+    from: 'ayoubsagh@gmail.com',
+    to: 'saghro@edu.uiz.ac.ma',
     subject: 'Test Email',
     html: '<h1>Hello from Node.js!</h1><p>This is a test email sent from Node.js using Nodemailer.</p>'
 };
